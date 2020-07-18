849095a9faa2c777a95625eb15cdcef6e478f29f
manuelbiolatiri <manuellaide49@gmail.com>
2020-07-18 22:53:41 +0100

    client build

---
+++ client/build/precache-manifest.12561af5404135262fc404b2d308d886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b359bb92d15129799944dae5cf8a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2f82cde25c92b042763",
+    "url": "/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "c25f05f12f6149141a97",
+    "url": "/static/css/main.b2571418.chunk.css"
+  },
+  {
+    "revision": "b2f82cde25c92b042763",
+    "url": "/static/js/2.5b57ad95.chunk.js"
+  },
+  {
+    "revision": "c25f05f12f6149141a97",
+    "url": "/static/js/main.a9bd2a6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
